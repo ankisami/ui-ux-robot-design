2e2c1d92f90560559ff6889872841cdfbd59e4d0
sami <sami.anki1316@gmail.com>
2024-11-06 20:51:37 +0100

    feat: add Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import { Header } from "./components";
+import { Header, Hero } from "./components";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
       <Header />
+      <Hero />
       <ButtonGradient />
     </>
   );
